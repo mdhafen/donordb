e60e6151c621bdfd3abdc4aca15c726e2c229591
Michael Hafen <michael.hafen@washk12.org>
2017-01-05 16:07:04 -0700

    Update copyright in footer for this year

--- htdocs/footer.php
+++ htdocs/footer.php
@@ -1,4 +1,4 @@
 <hr>
 <div class="uk-clearfix uk-text-small uk-text-center">
-Developed for <strong>Washington County School District</strong> by Michael Hafen. &copy; 2016
+Developed for <strong>Washington County School District</strong> by Michael Hafen. &copy; 2017
 </div>
